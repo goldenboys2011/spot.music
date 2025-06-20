498111c020dfc38047035d8420bb1a26b70bad86
Odysseas Chryssos <ody.chryssos@gmail.com>
2025-06-21 02:39:16 +0300

    Update logic.lua

--- logic.lua
+++ logic.lua
@@ -1,11 +1,8 @@
--- Your Icecast status endpoint
 local status_url = "https://rabbit-nights-collect-automobiles.trycloudflare.com/status-json.xsl"
 
--- Fetch JSON metadata from Icecast
 local res = fetch({
   url = status_url,
-  method = "GET",
-  headers = { ["Content-Type"] = "application/json" }
+  method = "GET"
 })
 
-print("HI")
+print(res)
