6fbf749c1c8105c79643f14a6873b9ea2b02666d
Odysseas Chryssos <ody.chryssos@gmail.com>
2025-06-21 20:21:40 +0300

    Update logic.lua

--- logic.lua
+++ logic.lua
@@ -1,16 +1,27 @@
 function update_status()
   local res = fetch({
-    url = "https://rabbit-nights-collect-automobiles.trycloudflare.com/status-json.xsl",
+    url = "https://zoo-hugo-civilization-japan.trycloudflare.com/status-json.xsl",
     method = "GET"
   })
 
   local np = get("now-playing", false)
-  local title = res.icestats.source.title
-  np.set_content("Now Playing: " .. title)
+  np.set_content("Now Playing: Unknown")
 
   local ls = get("listeners", false)
   local listeners = res.icestats.source.listeners
   ls.set_content(listeners .. " Listening Now!")
+
+  local cs = get("current-server", false)
+  local server = res.icestats.source.server_name
+  cs.set_content("Server: " .. server)
+
+  local ps = get("playing-since", false)
+  local since = res.icestats.source.stream_start
+  ps.set_content("Playing since: " .. since)
+
+  title = res.icestats.source.title
+  np.set_content("Now Playing: " .. title)
+
 end
 
 -- Schedule `update_status` to run every 10 seconds
