9aee32b24bacc4c9dd94bcb0933a6a9fd4301870
Odysseas Chryssos <ody.chryssos@gmail.com>
2025-06-21 03:18:28 +0300

    Update logic.lua

--- logic.lua
+++ logic.lua
@@ -9,10 +9,5 @@ local res = fetch({
 -- Try to get the "now-playing" element
 local np = get("now-playing", false)
 
--- Make sure both res and the element exist
-if np and type(res) == "table" and res.icestats and res.icestats.source and res.icestats.source.title then
-  local title = res.icestats.source.title
-  np.set_content("Now Playing: " .. title)
-else
-  print("Element not found or invalid responsee")
-end
+local title = res.icestats.source.title
+np.set_content("Now Playing: " .. title)
